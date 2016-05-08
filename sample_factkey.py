fea4120bf1124c08ee53129488a5dd7922bdc77a
Kevin Hanna <kevinhanna@gmail.com>
2016-05-08 15:18:03 -0600

    Updated instruction comments

--- sample_factkey.py
+++ sample_factkey.py
@@ -1,6 +1,7 @@
 from factual import Factual
 #https://factual.com/keys
-# rename to factkey.py
+# Copy to factkey.py and set variables
+# Do not commit this file to public repo with keys set
 
 factual_key = 'OAuth Key goes HERE'
 factual_secret = 'OAuth Secret goes HERE'
