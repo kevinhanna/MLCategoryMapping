5c96435144533b8c20d25713253f46b9987a1ed7
Kevin Hanna <khanna@vendasta.com>
2016-05-09 15:58:39 -0600

    Moved all ML code to new class Create simple_predictor.py to encapsulate simple prediction work

--- name_category_mapping.py
+++ name_category_mapping.py
@@ -1,11 +1,7 @@
 import csv
 import json
 
-import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
+from simple_predictor import SimplePredictor
 
 training_data_csv = open('data/factual_vtax_training_data.csv', 'r', encoding='utf-8', errors='ignore')
 business_names_csv = open('data/business_names.csv', 'r', encoding='utf-8', errors='ignore')
@@ -25,25 +21,11 @@ business_name_reader = csv.DictReader(business_names_csv, name_fieldnames)
 # Factual category ID's and labels
 factual_tax = json.load(factual_tax_json)
 
-def train(X_train, y_train):
-    if len(X_train) != len(y_train):
-        raise Exception('Training data and target data are of different lengths')
-    # Pipeline to vectorize text data and set classifier
-    classifier = Pipeline([
-        ('vectorizer', CountVectorizer()),
-        ('clf', OneVsRestClassifier(LinearSVC()))])
-    # Fit to data
-    classifier.fit(X_train, y_train)
-
-    return classifier
-
-
 def format_write(X_test, predicted, outfile):
     out = []
     for item, labels in zip(X_test, predicted):
         tmp_fid = factual_tax[str(labels)]["labels"]["en"]
         out.append([item, labels, tmp_fid])
-        #print('%s => %s - %s' % (item, labels, tmp_fid))
 
     writer = csv.writer(outfile, quoting=csv.QUOTE_ALL)
     writer.writerows(out)
@@ -56,6 +38,7 @@ test_names = set()
 unique_vtaxes = set()
 
 #names_out = []
+
 
 # Read training data Feature (vtax) Target (factual_category_ids) Test (unique_vtaxes)
 print("Formatting Data")
@@ -75,31 +58,28 @@ for name_row in business_name_reader:
     name = name_row['company_name']
     test_names.add(name)
 
-X_train_names = np.array(names)
-X_train_vtaxes = np.array(vtaxes)
-y_train = fids
 
+
+X_train_names = names
 X_test_names = sorted(test_names)
+
+X_train_vtaxes = vtaxes
 X_test_vtaxes = sorted(unique_vtaxes)
 
+y_train = fids
 
 # Predict and write names
 print("Learning names")
-name_classifier = train(X_train_names, y_train)
+names_kevifier = SimplePredictor(X_train_names, y_train)
 # 'predict' matching Factual category for name
-name_predicted = name_classifier.predict(test_names)
+name_predicted = names_kevifier.train(test_names)
+names_kevifier.dump('data/pickles/factualNameCategory.pkl')
 format_write(test_names, name_predicted, name_outfile)
-
 
 # Predict and write vtaxes
 print("Learning vtax")
-vtax_classifier = train(X_train_vtaxes, y_train)
+vtax_kevifier = SimplePredictor(X_train_vtaxes, y_train)
 # 'predict' matching Factual category for vtax
-vtax_predicted = vtax_classifier.predict(X_test_vtaxes)
+vtax_predicted = vtax_kevifier.train(X_test_vtaxes)
+vtax_kevifier.dump('data/pickles/factualCategory.pkl')
 format_write(X_test_vtaxes, vtax_predicted, vtax_outfile)
-
-
-# Persist training
-from sklearn.externals import joblib
-joblib.dump(name_classifier, 'data/pickles/factualNameCategory.pkl', 6)
-joblib.dump(vtax_classifier, 'data/pickles/factualCategory.pkl', 6)
---
+++ simple_predictor.py
@@ -0,0 +1,93 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+
+from sklearn.externals import joblib
+
+class SimplePredictor:
+
+
+    def __init__(self, sample_data, target_classifications):
+        """
+        
+        :param sample_data: Iterable of sample features (samples)
+        :param target_classifications: Iterable of target (expected classification from sample_data samples) classifications
+        """
+        self.__sample_data = sample_data
+        self.__target_classifications = target_classifications
+#        self.__to_predict = to_predict
+
+    def __getClassifier(self):
+        return self.__classifier
+
+    def __setClassifier(self, classifier):
+        self.__classifier = classifier
+
+    def getPredictor(self):
+        return self.__predictor
+
+    def __setPredictor(self, predictor):
+        self.__predictor = predictor
+
+
+    def train(self, to_predict):
+        """
+        Creates mapping from sample_data (features) to target_classifications (classes) and then predicts target_classifications values for to_predict
+        - sample_data and target_classifications need to be same length
+        - to_predict should have similar data as sample_data
+
+        :param to_predict:  Iterable of items to predict classification, these must be of the same type as sample_data
+        :return: Iterable of predicted classifications for each to_predict
+        """
+
+        if len(self.__sample_data) != len(self.__target_classifications):
+            #TODO There's probably a more appropriate Exception type
+            raise Exception('Training data and target data are of different lengths')
+
+        sample_data_np = self.__sample_data #np.array(sample_data) TODO is np.array better?
+
+#TODO        if not self.__getClassifier():
+        self.__setClassifier(self.__getPipeline(self.__sample_data))
+
+        # Fit to data
+        self.__getClassifier().fit(sample_data_np, self.__target_classifications)
+
+        return self.__predict(to_predict)
+
+
+    def __predict(self, test_data):
+        if not self.__classifier:
+            #TODO There's probably a more appropriate Exception type
+            raise Exception("Must run train before calling predict")
+
+        self.__setPredictor(self.__getClassifier().predict(test_data))
+
+        return self.getPredictor()
+
+    def __getPipeline(self, sample_data):
+        """
+
+        :param sample_data:
+        :return:
+        """
+
+        # to accept either a list of strings ['one', 'two', 'three'] or a list of lists [[1,2], [3,4]]
+        if isinstance(sample_data[0], str):
+            # Pipeline to vectorize text data and set classifier
+            pipeline = Pipeline([
+                ('vectorizer', CountVectorizer()),
+                ('clf', OneVsRestClassifier(LinearSVC()))])
+        elif hasattr(sample_data[0], '__iter__'):
+            # Pipeline to set classifier
+            pipeline = Pipeline([
+                ('clf', OneVsRestClassifier(LinearSVC()))])
+        else:
+            raise Exception("Unable to use types stored in sample_data:" + type(sample_data))
+
+        return pipeline
+
+    def dump(self, target):
+        joblib.dump(self.__getClassifier(), target, 6)
+
