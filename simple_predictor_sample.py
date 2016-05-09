14cfdfd46645f4547251a136537c161ddc7aa974
Kevin Hanna <khanna@vendasta.com>
2016-05-09 16:19:04 -0600

    Updated tests

--- simple_predictor_sample.py
+++ simple_predictor_sample.py
@@ -1,18 +1,23 @@
 from simple_predictor import SimplePredictor
 
+print("Running test 1")
 sample = [[2,1], [2,2], [1,1], [1,2]]
-test = [[2,1], [1,1]]
-
-sample2 = ['foo bar', 'foo', 'dave', 'john']
-test2 = ['foo', 'bob']
-
 classifications = [1, 1, 0, 0]
+test1 = [[2, 1], [1, 1]]
+test1_1 = [[1, 1], [2, 1]]
+simplePredictor = SimplePredictor(sample, classifications)
 
-kev = SimplePredictor(sample, classifications)
-kev2 = SimplePredictor(sample2, classifications)
+print("Tes1 1.1: %s" % simplePredictor.train(test1))
+print("Test 1.2: %s" % simplePredictor.train(test1_1))
 
-result = kev.train(test)
-result2 = kev2.train(test2)
+print("Running test 2")
+sample2 = ['foo bar', 'foo', 'dave', 'john']
+classifications2 = [1, 1, 0, 0]
+test2 = ['foo', 'bob']
+test2_1 = ['bob', 'foo']
+simplePredictor2 = SimplePredictor(sample2, classifications2)
 
-print(result)
-print(result2)
+print("Tes1 2.1: %s" % simplePredictor2.train(test2))
+print("Test 2.2: %s" % simplePredictor2.train(test2_1))
+
+
